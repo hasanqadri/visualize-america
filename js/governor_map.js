14e29d92e972cb03c2929fa4e2f9432956c10779
Hasan Qadri <hqadri6@gatech.edu>
2018-07-19 18:29:32 -0400

    Added circles as centroids to reperesent incumbents to states. Binded RCP data to states.

--- js/governor_map.js
+++ js/governor_map.js
@@ -5,7 +5,7 @@ governorCount = 0;
 function governor_map()
 {
     var width = 1500,
-        height = 800;
+        height = 700;
 
     var projection = d3.geo.albersUsa()
         .scale(1500)
@@ -40,7 +40,6 @@ function governor_map()
     }
 
     function initialState(d) {
-        console.log(governorD);
         for (var x = 0; x < governorD.length; x++) {
             if (d.properties.STATE_ABBR === governorD[x].state_code) {
                 if (governorD[x].party === "democrat") {
@@ -60,7 +59,7 @@ function house_map()
 {
 
     var width = 1500,
-        height = 800;
+        height = 700;
 
     var projection = d3.geo.albersUsa()
         .scale(1500)
@@ -129,7 +128,7 @@ function house_map()
 
 function senate_map() {
     var width = 1500,
-        height = 800;
+        height = 700;
 
     var projection = d3.geo.albersUsa()
         .scale(1500)
@@ -144,16 +143,18 @@ function senate_map() {
 
     var usD = null;
     var senateD = null;
+    var rcpD = null;
 
     queue()
         .defer(d3.json, "./Data/us2.json")
         .defer(d3.json, "./Data/us-senate.json")
+        .defer(d3.json, "./Data/rcp-senate-abbr.json")
         .await(ready);
 
-    function ready(error, us, senate) {
+    function ready(error, us, senate, rcp) {
         usD = us;
         senateD = senate;
-
+        rcpD = rcp;
         svg.selectAll('.states')
             .data(topojson.feature(us, us.objects.usStates).features)
             .enter()
@@ -163,17 +164,29 @@ function senate_map() {
             .attr("fill", initialState)
             .attr("stroke", '#fff')
 
+        svg.append("g")
+            .attr("class", "bubble")
+            .selectAll("circle")
+            .data(topojson.feature(us, us.objects.usStates).features)
+            .enter().append("circle")
+            .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+            .attr("r", 4)
+            .attr("fill", initialStateCircle)
+            .style('stroke', 'black')
+            .style('stroke-width', 1);
+
     }
     function initialState(d) {
-        var dualStates = ['NV', 'CO',  'MT', 'ND', 'WI', 'MO', 'IN', 'OH', 'WV', 'PA', 'ME', 'AL', 'FL'];
+        /**var dualStates = ['NV', 'CO',  'MT', 'ND', 'WI', 'MO', 'IN', 'OH', 'WV', 'PA', 'ME', 'AL', 'FL'];
         for (var z = 0; z < dualStates.length; z++) {
             if (d.properties.STATE_ABBR === dualStates[z]) {
                 return '#9c1ecb'
             }
         }
-
-        for (var x = 0; x < senateD.results[0].members.length; x++) {
-            if (d.properties.STATE_ABBR === senateD.results[0].members[x].state) {
+        **/
+        console.log(rcpD[0].state)
+        for (var x = 0; x < 20; x++) {
+            if (d.properties.STATE_ABBR === rcpD[x].state) {
                 if (senateD.results[0].members[x].party === "D") {
                     return '#084594'
                 } else {
@@ -181,5 +194,20 @@ function senate_map() {
                 }
             }
         }
+        return '#807d85'
+    }
+
+    function initialStateCircle(d) {
+        for (var x = 0; x < senateD.results[0].members.length; x++) {
+            if (d.properties.STATE_ABBR === senateD.results[0].members[x].state) {
+                if (senateD.results[0].members[x].party === "D") {
+                    return '#084594'
+                } else {
+                    return '#cb181d'
+
+                }
+            }
+        }
+        return '#807d85'
     }
 }
